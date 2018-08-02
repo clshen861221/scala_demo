1621ab1eacfdf4fa3958a58653c34c0f61a1f74b
clshen861221 <547393465@qq.com>
2018-08-02 21:26:00 +0800

    Add problem

---
+++ test-model/src/main/scala/com/nielsen/scala_demo/problem/BJImpr.scala
@@ -0,0 +1,57 @@
+package com.nielsen.scala_demo.problem
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+object BJImpr {
+  def main(args: Array[String]): Unit = {
+    System.setProperty("hadoop.home.dir", "D:/study/Hadoop/hadoop-common-2.2.0-bin-master")
+    val sparkSession = SparkSession.builder()
+      .master("local")
+      .appName("ImprCalculation")
+      .getOrCreate()
+    val reader = sparkSession.read.option("header", "true").option("sep", "|")
+    //    val imprDf = reader.csv("C:/Users/shhu6001/Downloads/data/2018-01-25/NORMAL/FB/2072,6.csv")
+    //    val dmaDf = reader.csv("C:/Users/shhu6001/Downloads/data/STATIC_DATA/DESIGNATED_MARKET_AREA.csv").filter(col("DESIGNATED_MARKET_AREA_NAME") === "Beijing")
+    //    val cmpgDf = reader.csv("C:/Users/shhu6001/Downloads/data/2018-01-25/METADATA/campaign.csv")
+
+    val impr28Df = reader.csv("C:/Users/shhu6001/Downloads/data/2018-01-28/NORMAL/FB/*.csv")
+    val impr25Df = reader.csv("C:/Users/shhu6001/Downloads/data/2018-01-25/NORMAL/FB/*.csv")
+
+    import sparkSession.implicits._
+    //    dmaDf.select("REPORTABLE_DESIGNATED_MARKET_AREA_ID", "DESIGNATED_MARKET_AREA_NAME").show
+    //    val result = imprDf.as("impr")
+    //      .join(dmaDf.as("dma"), col("dma.REPORTABLE_DESIGNATED_MARKET_AREA_ID") === col("impr.DESIGNATED_MARKET_AREA_ID"))
+    //      .join(cmpgDf, "CAMPAIGN_ID")
+    //      .groupBy("CAMPAIGN_ID", "CAMPAIGN_NAME")
+    //      .agg(sum("IMPRESSIONS").as("IMPRESSIONS"))
+    //      .as[TestData]
+
+    val result = impr28Df.as("impr_28")
+                .groupBy("impr_28.PLACEMENT_ID")
+                .agg(sum("impr_28.IMPRESSIONS").as("total_impr_28"))
+                .as("total_28")
+                .join(impr25Df.as("impr_25")
+                      .groupBy("impr_25.PLACEMENT_ID")
+                      .agg(sum("impr_25.IMPRESSIONS").as("total_impr_25"))
+                      .as("total_25"), col("total_28.PLACEMENT_ID") === col("total_25.PLACEMENT_ID"))
+                .where(col("total_28.total_impr_28") < (col("total_25.total_impr_25")+ 100000))
+                .select("total_28.PLACEMENT_ID","total_28.total_impr_28","total_25.total_impr_25")
+                .as[TestData]
+    
+    result.show()
+//    result.foreach(println(_))
+
+  }
+
+}
+
+case class TestData(
+  PLACEMENT_ID:   String,
+  total_impr_28: String,
+  total_impr_25: String
+  
+//  ,
+//  CAMPAIGN_NAME: String,
+//  IMPRESSIONS:   Double
+  )
